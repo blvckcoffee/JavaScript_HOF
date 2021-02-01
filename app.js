e9ef48acc660fc509e3dc4b6286ad2eff23c8d01
blvckcoffee <joe.johnson.dev@gmail.com>
2021-02-01 13:53:30 -0600

    completed exercise

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-console.log("Hello World!\n==========\n");
+console.log('Hello World!\n==========\n');
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log('EXERCISE 1:\n==========\n');
+
+const plus = (number) => {
+  return (plusNumber) => plusNumber + number;
+};
+
+let plus15 = plus(15);
+
+console.log(plus15(10));
